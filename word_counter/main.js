55b86d719b7907fe5d63ac200a8cae7157eacbb7
Juna <59763513+KiwiDevLive@users.noreply.github.com>
2020-10-15 23:41:35 +1300

    word counter application functional
    functions in place, only thing that needs adding is slight styling changes.

---
+++ word_counter/main.js
@@ -0,0 +1,18 @@
+/// Word Counter
+function calculateWordCount() {
+  var resultsText = document.getElementById("resultsText");
+  var input = document.getElementById("inputbox");
+  var words = input.value.split(" ");
+  for(var _i = words.length-1; _i >= 0; --_i) {
+    if(words[_i] == "") {
+      words.splice(_i, 1);
+    }
+  }
+  resultsText.innerText = String(words.length) + " Words";
+}
+
+function clearText() {
+  console.log("hi");
+  var input = document.getElementById("inputbox");
+  input.value = "";
+}
